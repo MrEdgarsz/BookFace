497faf9b087de9300fa14eef6a2efceeafa4b04e
Dminik Edgar Szpilski <kontaktedgarsz@gmail.com>
2022-04-06 20:45:45 +0200

    renamed auth_module to auth

--- bookface/__init__.py
+++ bookface/__init__.py
@@ -3,13 +3,13 @@ from flask import Flask, render_template
 from flask_bcrypt import Bcrypt
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
-from bookface.auth import auth_module
+from bookface.auth import auth
 
 app = Flask(__name__, template_folder="pages")
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///condition_monitor.db"
 app.config["SECRET_KEY"] = "97fdf98cb2f69976dc8c7c17"
-app.register_blueprint(auth_module)
+app.register_blueprint(auth)
 bcrypt = Bcrypt(app)
 db = SQLAlchemy(app)
 login_manager = LoginManager(app)
