78bc5717a0ee1371ea261e43e84ff6daca7e0a2f
Doic <michalakradek2@gmail.com>
2022-04-13 19:56:32 +0200

    added post update date

--- bookface/posts/models/posts.py
+++ bookface/posts/models/posts.py
@@ -6,7 +6,8 @@ class Post(db.Model):
     __tablename__ = "posts"
     post_id = db.Column(db.Integer, primary_key=True)
     description = db.Column(db.Text)
-    creation_date = db.Column(db.DateTime, default=datetime.now())
+    created_at = db.Column(db.DateTime, default=datetime.now())
+    updated_at = db.Column(db.DateTime, default=datetime.now())
     #user_id = db.Column(db.Integer, ForeignKey('user.user_id'))
 
     def __init__(self, *args, **kwargs):
