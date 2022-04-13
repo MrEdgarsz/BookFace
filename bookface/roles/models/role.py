73c135be00d953313391eadb48ba103a06fe9554
Daniel <daniel.oblak99@gmail.com>
2022-04-13 20:23:02 +0200

    Renamed file roles=>role, changed name of class Roles=>Role, changed table name to lower case roles

--- bookface/roles/models/role.py
+++ bookface/roles/models/role.py
@@ -2,8 +2,8 @@ from bookface import db
 from sqlalchemy import Table, Column, Integer, Boolean, String, ForeignKey, MetaData
 
 
-class Roles(db.Model):
-    __tablename__ = "Roles"
+class Role(db.Model):
+    __tablename__ = "roles"
     role_id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(10), nullable=False)
     can_add_post = db.Column(db.Boolean, nullable=False)
@@ -11,7 +11,7 @@ class Roles(db.Model):
     can_remove_post = db.Column(db.Boolean, nullable=False)
 
     def __init__(self, *args, **kwargs):
-        super(Roles, self).__init__(*args, **kwargs)
+        super(Role, self).__init__(*args, **kwargs)
 
 
 # define relationships with user model: https://flask-sqlalchemy.palletsprojects.com/en/2.x/quickstart/#a-minimal-application
