e5752e3b3a0fc48d3e95b9fc5452d281a049b635
Dminik Edgar Szpilski <kontaktedgarsz@gmail.com>
2022-04-13 19:27:08 +0200

    Added Flask-bootstrap

--- bookface/__init__.py
+++ bookface/__init__.py
@@ -1,26 +1,24 @@
 from pydoc import render_doc
 from flask import Flask, render_template
 from flask_bcrypt import Bcrypt
+from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from bookface.auth import auth
+from bookface.auth import routes
 
 app = Flask(__name__, template_folder="pages")
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///condition_monitor.db"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///bookface.db"
 app.config["SECRET_KEY"] = "97fdf98cb2f69976dc8c7c17"
 app.register_blueprint(auth)
+bootstrap = Bootstrap(app)
 bcrypt = Bcrypt(app)
 db = SQLAlchemy(app)
-login_manager = LoginManager(app)
-login_manager.login_view = "login_page"
-login_manager.login_message_category = "info"
 
-
-@login_manager.user_loader
-def load_user(user_id):
-    return None;
 
 @app.route("/")
 def home_page():
     return render_template("home.html")
+
+
