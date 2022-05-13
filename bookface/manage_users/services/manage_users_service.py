f0b3ab6aad020a68132ad1cca4b0081c35426894
Daniel <daniel.oblak99@gmail.com>
2022-05-13 20:28:22 +0200

    Implemented ManageUsersService with methods to manipulate users db

---
+++ bookface/manage_users/services/manage_users_service.py
@@ -0,0 +1,28 @@
+from bookface import db
+from bookface.auth.models.user import User
+from bookface.misc import singleton
+from bookface.roles.models.role import Role
+
+
+
+class ManageUsersService(metaclass=singleton.Singleton):
+    def __init__(self):
+        self.session=db.session
+    
+    def get_all_users(self):
+        return User.query.all()
+    
+    def ban_user(self,user_id):
+        user = self.session.query(User).filter_by(id=user_id).first()
+        role = self.session.query(Role).filter_by(name="blocked_user").first()
+        user.role = role
+        return self.session.merge(user)
+
+    def unban_user(self, user_id):
+        user = self.session.query(User).filter_by(id=user_id).first()
+        role = self.session.query(Role).filter_by(name="user").first()
+        user.role = role
+        return self.session.merge(user)
+
+    def flush(self):
+        self.session.commit()
