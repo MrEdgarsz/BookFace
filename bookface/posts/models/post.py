a7fe6c567a912759c2b0c68c9937af04544588ff
Doic <michalakradek2@gmail.com>
2022-05-15 11:18:44 +0200

    added func to calculate time difference for post

--- bookface/posts/models/post.py
+++ bookface/posts/models/post.py
@@ -1,8 +1,6 @@
 from bookface import db
 from sqlalchemy.orm import backref
-from datetime import datetime
-from sqlalchemy import ForeignKey
-from bookface.auth.models.user import User
+from datetime import datetime, timedelta
 
 class Post(db.Model):
     __tablename__ = "posts"
@@ -16,3 +14,30 @@ class Post(db.Model):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+
+    def get_time_difference(self):
+        time_difference = datetime.now() - self.created_at
+        time_difference_sec = time_difference.seconds
+
+        MIN = 60
+        HOUR = 60*MIN
+        DAY = 24*HOUR
+        MONTH = 30*DAY
+        YEAR = 12*MONTH
+
+        if (time_difference_sec < MIN):
+            result = "Przed chwilą"
+        elif (time_difference_sec >= MIN and time_difference_sec < HOUR):
+            result = f"{int(time_difference_sec/MIN)} min temu"
+        elif (time_difference_sec >= HOUR and time_difference_sec < DAY):
+            result = f"{int(time_difference_sec/HOUR)} godz. temu"
+        elif (time_difference_sec >= DAY and time_difference_sec < (2*DAY)):
+            creation_date = self.created_at.strftime("%H:%M")
+            result = f"Wczoraj o {creation_date}"
+        elif (time_difference_sec >= (2*DAY) and time_difference_sec < MONTH):
+            result = time_difference.strftime("%d.%B %H:%M")
+        elif (time_difference_sec >= MONTH and time_difference_sec < YEAR):
+            result = time_difference.strftime("%d.%B")
+        else:
+            result = time_difference.strftime("%d %B %Y")
+        return result
