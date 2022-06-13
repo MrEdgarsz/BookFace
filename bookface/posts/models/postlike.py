f637bf4fae6586aff8f991cf600696e0110ce99c
Doic <michalakradek2@gmail.com>
2022-06-13 16:34:32 +0200

    created model for post likes

---
+++ bookface/posts/models/postlike.py
@@ -0,0 +1,11 @@
+from bookface import db
+from sqlalchemy.orm import backref
+
+class PostLike(db.Model):
+    __tablename__ = "postslikes"
+    id = db.Column(db.Integer, primary_key=True)
+    post_id = db.Column(db.Integer, db.ForeignKey("posts.id"))
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"))
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
