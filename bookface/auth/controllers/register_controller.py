47a7fbffe501b27fb27efb668d54f4fe167bff30
Daniel <daniel.oblak99@gmail.com>
2022-06-13 22:11:02 +0200

    Added flash snackbar after register

--- bookface/auth/controllers/register_controller.py
+++ bookface/auth/controllers/register_controller.py
@@ -1,5 +1,4 @@
-import flask
-from flask import render_template, url_for
+from flask import render_template, url_for, flash
 from werkzeug.utils import redirect
 
 from bookface.auth.forms.register_form import RegisterForm
@@ -12,6 +11,6 @@ def handle_sign_up():
     if form.validate_on_submit():
         UserService().create(user=User(username=form.username.data, password=form.password.data,role_id=3))
         form.data.clear()
-        flask.flash("You have successfully registered! you can now sign in.", "success")
+        flash("Rejestracja powiodła się! Możesz się teraz zalogować.", "success")
         return redirect(url_for("auth.sign_in"))
     return render_template("sign_up_page.html", form=form)
